f9ea726875cec0c71b2e91187e4ed28bdf500840
Brad Broge <brbroge@gmail.com>
2017-08-25 14:26:35 -0400

    updating headers

--- src/Components/Header/Tracker.js
+++ src/Components/Header/Tracker.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import '../../styles/header.css';
 
 class Tracker extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      health: '',
-      }
-  }
-
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
+    const stats = this.props.stats;
+    const display = stats.map((stat) => {
+      if (stat.name === "helpers") {
+        return (
+          <div key={stat.name} className={stat.name}>
+            <strong>{stat.name}:</strong>
+          </div>
+        )
+      }
+      return (
+        <div key={stat.name} className={stat.name}>
+          <strong>{stat.name}:</strong> {stat.value}
+        </div>
+      )
+    })
     return (
 
-      <div className= "healthContainer">
-      This is Health
+    <div className= "statContainer">
+      {display}
     </div>
     )
   }
